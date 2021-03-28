f913eb6a0f073956188ffca3f89ea294816778dc
Michael Cunningham <michaelbcunningham51@gmail.com>
2021-03-28 13:10:11 -0400

    added map with default to tink and checkboxes

--- case-place/src/App.js
+++ case-place/src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
+import GoogleMapReact from 'google-map-react';
+import React, { useState, Component} from 'react';
 import './App.css';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
+import ToggleButton from 'react-bootstrap/ToggleButton'
+
 
 function App() {
+  const [value, setValue] = useState(1);
+  const handleChange = (val) => setValue(val);
+  const AnyReactComponent = ({ text }) => <div>{text}</div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+    <ToggleButtonGroup type="checkbox" value={value} exclusive onChange={handleChange}>
+      <ToggleButton value={1}>Explore</ToggleButton>
+      <ToggleButton value={2}>Food</ToggleButton>
+      <ToggleButton value={3}>Directions</ToggleButton>
+      <ToggleButton value={4}>Transport</ToggleButton>
+    </ToggleButtonGroup>
+    <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyBqiKidu6V4PTb8YZJdQET9qVjYTWFSG1o"}}
+          defaultCenter={{lat:41.508186,lng:-81.608665}}
+          defaultZoom={17}
         >
-          Learn React
-        </a>
-      </header>
+          <AnyReactComponent
+            lat={41.508186}
+            lng={-81.608665}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
     </div>
+    
   );
 }
 
