1497a75fe6a981fe6507e0aa07b9c220a784a950
Rishik Hombal <hombalrishik@gmail.com>
2021-03-31 18:40:10 -0400

    added hover styling and cleaned up code

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import './styles/App.scss';
 import ButtonModal from "./components/ButtonModal";
 
 function App() {
-  // #TODO: Assign into local storage to prevent refire
-  const [showWelcomeModal, setShowWelcomeModal] = useState(true);
-  const [welcomeInfoData, setWelcomeInfoData] = useState({title: 'I am a', selection: ''});
-  return (
-      <div className={"App"}>
-        <div className={showWelcomeModal ? 'dim-background' : ''} />
-        {showWelcomeModal && (
-            <ButtonModal showWelcomeModal={showWelcomeModal} setShowWelcomeModal={setShowWelcomeModal}
-                         buttonTitles={['Parent or Prospective Student', 'Guest', 'Current Student']}
-                         welcomeInfoData={welcomeInfoData}
-                         setWelcomeInfoData={setWelcomeInfoData}/>
-        )}
-      </div>
-  );
+    // #TODO: Assign into local storage to prevent refire
+    const [showWelcomeModal, setShowWelcomeModal] = useState(true);
+    const [welcomeInfoData, setWelcomeInfoData] = useState('');
+
+    return (
+        <div className={"App"}>
+            <div className={showWelcomeModal ? 'dim-background' : ''}/>
+            {showWelcomeModal && (
+                <ButtonModal modalTitle={'I am a'} showWelcomeModal={showWelcomeModal}
+                             setShowWelcomeModal={setShowWelcomeModal}
+                             buttonTitles={['Parent or Prospective Student', 'Guest', 'Current Student']}
+                             welcomeInfoData={welcomeInfoData}
+                             setWelcomeInfoData={setWelcomeInfoData}/>
+            )}
+        </div>
+    );
 }
 
 export default App;
--- src/components/ButtonModal.js
+++ src/components/ButtonModal.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-const ButtonModal = ({setShowWelcomeModal, buttonTitles, welcomeInfoData, setWelcomeInfoData}) => {
+const ButtonModal = ({modalTitle, setShowWelcomeModal, buttonTitles, welcomeInfoData, setWelcomeInfoData}) => {
     const onButtonClickHandler = (title) => {
         setShowWelcomeModal(false);
-        setWelcomeInfoData({title: welcomeInfoData.title, selection: title});
+        setWelcomeInfoData(title);
     }
 
     return (
         <div className={"buttonContainer"}>
-        <h1>{welcomeInfoData.title}</h1>
+        <h1>{modalTitle}</h1>
             {buttonTitles.map((title) => {
                 return (
                     <button onClick={() => onButtonClickHandler(title)} key={title}>
