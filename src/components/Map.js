a5a0b1a7da5638a707a33c705d18fde34b4b75b3
Michael Cunningham <michaelbcunningham51@gmail.com>
2021-04-05 20:32:25 -0400

    updated so easy to add in filtering by buttons

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,35 +5,35 @@ import places from './locations.json'
 import GoogleMapReact from 'google-map-react';
 const Map = () => {
     
-    const [value, setValue] = useState(1);
-    const handleChange = (val) => setValue(val);
+    
   
     const AnyReactComponent = ({ text }) => <div>{text}</div>;
-    const locations = places.map(data =>
-        <AnyReactComponent
+    var locations = places.map(data => {
+      if(data.category == "CSE") 
+        return (<AnyReactComponent
            lat = {data.latitude}
             lng = {data.longitude}
-            text = {data.name} ></AnyReactComponent>
+            text = {data.name} ></AnyReactComponent>)
+    }
       )
+
+
 
       return (
         <div>
-        <ToggleButtonGroup type = "checkbox" exclusive>
-            <ToggleButton value = "Explore">Explore</ToggleButton>
-            <ToggleButton value = "Transportation">Transportation</ToggleButton>
-            <ToggleButton value = "Directions">Directions</ToggleButton>
-            <ToggleButton value = "Food">Food</ToggleButton>
-        </ToggleButtonGroup>
+            <div>
+        <input type="radio" value="Housing" name="gender" /> Housing
+        <input type="radio" value="Food" name="gender" /> Food
+        <input type="radio" value="Medical" name="gender" /> Medical
+        <input type="radio" value="Phys" name="gender" /> Phys
+        <input type="radio" value="Services" name="gender" /> Services
+      </div>
         <div style={{ height: '100vh', width: '100%' }}>
             <GoogleMapReact
               bootstrapURLKeys={{ key: "AIzaSyBqiKidu6V4PTb8YZJdQET9qVjYTWFSG1o"}}
               defaultCenter={{lat:41.508186,lng:-81.608665}}
               defaultZoom={17}
             >
-                <AnyReactComponent
-             lat = {places[1].latitude}
-              lng = {places[1].longitude}
-              text = {places[1].name} ></AnyReactComponent>
                   {locations}
             </GoogleMapReact>
           </div>
