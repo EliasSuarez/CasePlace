2bfbef76b85d86da1e9b68499783c36653f6652c
Rishik Hombal <hombalrishik@gmail.com>
2021-04-19 19:39:51 -0400

    updated json with info tag

--- src/util/SampleMapPinData.js
+++ src/util/SampleMapPinData.js
@@ -1,19 +1,18 @@
-import React from "react"
-
 export const PinData = [
-    {
-        name: "Veale Center",
-        info: "A cool gym near southside. It has some really cool amenities. You should check it out!",
-        open: false
-    },
-    {
-        name: "Leutner",
-        info: "A cafeteria near northside",
-        open: false
-    },
-    {
-        name: "Clarke Tower",
-        info: "A dorm near northside",
-        open: false
-    }
-]
+  {
+    name: "Veale Center",
+    info:
+      "A cool gym near southside. It has some really cool amenities. You should check it out!",
+    open: false,
+  },
+  {
+    name: "Leutner",
+    info: "A cafeteria near northside",
+    open: false,
+  },
+  {
+    name: "Clarke Tower",
+    info: "A dorm near northside",
+    open: false,
+  },
+];
